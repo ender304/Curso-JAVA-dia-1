897881430a29b1f7a9b3fc04cf71843fd0746e69
laoratorio <laoratorio@atl-labobd-312>
2015-06-15 16:06:28 -0430

    Ejercicio 1 del curso de JAVA

---
+++ JavaApplication1/src/javaapplication1/ClaseTres.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author laoratorio
+ */
+public class ClaseTres {
+    protected String prot = "ejemplo prote";
+    private int i = 2;
+    public int j = 3;
+}
--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -1,5 +1,8 @@
 package javaapplication1;
 
+import nuevoPaquete.ClaseDos;
+
+ 
 public class JavaApplication1 {
     private int contador;
 
@@ -11,6 +14,13 @@ public class JavaApplication1 {
         for (jp1.contador = 0; jp1.contador < 10; jp1.contador++) {
             System.out.println("imprimiendo el contador \n\n\n" +jp1.contador);
         }
+        
+        ClaseDos cd1 = new ClaseDos();
+        System.out.println("atributos clasedos "+cd1.otraCosa);
+        
+        ClaseTres ct1 = new ClaseTres();
+        System.out.println("atributos clasetres "+ct1.prot);
+        System.out.println("atributos clasetres "+ct1.j);
 
     }
 
